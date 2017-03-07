f593ce6964f20b9e7ef07a33909fb63300fa3822
asa10yoru <asa10yoru@techacademy.jp>
2017-03-07 20:58:39 +0900

    ボタンの位置を修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,9 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func onPrev(sender: AnyObject) {
+        // 表示している画像の番号を1減らす
+        dispImageNo -= 1
+        
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+    }
+    
+    @IBAction func onNext(sender: AnyObject) {
+        
+        // 表示している画像の番号を1増やす
+        dispImageNo += 1
+        
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+    }
+    
+    /// 表示している画像の番号
+    var dispImageNo = 0
+    
+    /// 表示している画像の番号を元に画像を表示する
+    func displayImage() {
+        
+        // 画像の名前の配列
+        let imageNameArray = [
+            "img1",
+            "img2",
+            "img3"
+            ]
+        // 画像の番号が正常な範囲を指しているかチェック
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "img1")
+        imageView.image = image
     }
 
     override func didReceiveMemoryWarning() {
